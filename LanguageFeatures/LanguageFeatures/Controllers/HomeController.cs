72e0fe92a0b2ca138734ea001da1938f5c5f29db
katlego Maphango <mphangokatlego08@gmail.com>
2023-03-13 22:22:39 +0200

    Created languagesFeatures mvc app

---
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using LanguageFeatures.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
